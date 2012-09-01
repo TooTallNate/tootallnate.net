8dd7faafcbe1ddaa500e0cfb4c78f7417fcd120c
Nathan Rajlich <nathan@tootallnate.net>
2012-09-01 16:37:13 -0700

    use "http" for now

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var code = 301; // Permenant Redirect
+var secure = false;
 http.createServer(function (req, res) {
   res.statusCode = code;
-  res.setHeader('Location', 'https://n8.io' + req.url);
+  res.setHeader('Location', 'http' + (secure ? 's': '') + '://n8.io' + req.url);
   res.end();
 }).listen(8080);
